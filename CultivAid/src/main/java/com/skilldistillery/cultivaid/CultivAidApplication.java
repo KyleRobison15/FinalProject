436ad58ac4881fcfd3bf3d8309e2b4dff279187b
Matthew Lee <matthewlee686@gmail.com>
2021-08-17 16:13:56 -0400

    Stub Spring Boot/REST Project, connect JPA Project dependency

---
+++ CultivAid/src/main/java/com/skilldistillery/cultivaid/CultivAidApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.cultivaid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CultivAidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CultivAidApplication.class, args);
+	}
+
+}
---
+++ CultivAid/src/test/java/com/skilldistillery/cultivaid/CultivAidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.cultivaid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CultivAidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
