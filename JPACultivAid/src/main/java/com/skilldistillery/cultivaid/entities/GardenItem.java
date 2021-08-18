8e80902b2775a0ac38896eaef98832f22da849d8
livesimple02 <shlaupan@yahoo.com>
2021-08-17 21:51:39 -0600

    Mapped basic entities and Junit for ExchangeImage, GardenItem, GardenItemComment. Mapped relationships between GardenItems and GardenItemComments & Junit tested.

---
+++ JPACultivAid/src/main/java/com/skilldistillery/cultivaid/entities/GardenItem.java
@@ -0,0 +1,236 @@
+package com.skilldistillery.cultivaid.entities;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+@Table (name="garden_item")
+public class GardenItem {
+
+//////////////////////////FIELDS //////////////////////////////
+
+	@Id
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String description;
+	
+	@Column (name="grow_method")
+	private String growMethod;
+	
+	@Column (name="date_expected")
+	private String dateExpected;
+	
+	private int amount;
+	
+	private String variety;
+	
+	private boolean pesticides;
+	
+	private boolean fertilizers;
+	
+	@Column (name="create_date")
+	@CreationTimestamp
+	private LocalDateTime createdDate;
+	
+	@JsonIgnore
+	@OneToMany (mappedBy="gardenItem")
+	private List<GardenItemComment> gardenItemComments;
+
+	
+	
+//////////////////////////CONSTRUCTORS //////////////////////////////
+
+	public GardenItem() {
+		super();
+	}
+
+
+//////////////////////////GETTERS SETTERS //////////////////////////////
+
+	public int getId() {
+		return id;
+	}
+
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+
+	public String getDescription() {
+		return description;
+	}
+
+
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+
+
+	public String getGrowMethod() {
+		return growMethod;
+	}
+
+
+
+	public void setGrowMethod(String growMethod) {
+		this.growMethod = growMethod;
+	}
+
+
+
+	public String getDateExpected() {
+		return dateExpected;
+	}
+
+
+
+	public void setDateExpected(String dateExpected) {
+		this.dateExpected = dateExpected;
+	}
+
+
+
+	public int getAmount() {
+		return amount;
+	}
+
+
+
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+
+
+
+	public String getVariety() {
+		return variety;
+	}
+
+
+
+	public void setVariety(String variety) {
+		this.variety = variety;
+	}
+
+
+
+	public boolean isPesticides() {
+		return pesticides;
+	}
+
+
+
+	public void setPesticides(boolean pesticides) {
+		this.pesticides = pesticides;
+	}
+
+
+
+	public boolean isFertilizers() {
+		return fertilizers;
+	}
+
+
+
+	public void setFertilizers(boolean fertilizers) {
+		this.fertilizers = fertilizers;
+	}
+
+
+
+	public LocalDateTime getCreatedDate() {
+		return createdDate;
+	}
+
+
+
+	public void setCreatedDate(LocalDateTime createdDate) {
+		this.createdDate = createdDate;
+	}
+
+
+	
+	public List<GardenItemComment> getGardenItemComments() {
+		return gardenItemComments;
+	}
+
+
+	public void setGardenItemComments(List<GardenItemComment> gardenItemComments) {
+		this.gardenItemComments = gardenItemComments;
+	}
+
+	
+	public List<GardenItemComment> addGardenItemComment(GardenItemComment gic) {
+		if (this.gardenItemComments == null || this.gardenItemComments.size() == 0) {
+			this.gardenItemComments = new ArrayList<>();
+		}
+		if (!this.gardenItemComments.contains(gic)) {
+			this.gardenItemComments.add(gic);
+		}
+		return this.gardenItemComments;		
+	}
+	
+	public void removeGardenItemComment(GardenItemComment gic) {
+		if (this.gardenItemComments != null && this.gardenItemComments.contains(gic)) {
+			this.gardenItemComments.remove(gic);
+		}
+	}
+	
+	
+//////////////////////////TO STRING //////////////////////////////
+
+	@Override
+	public String toString() {
+		return "GardenItem [id=" + id + ", description=" + description + ", growMethod=" + growMethod
+				+ ", dateExpected=" + dateExpected + ", amount=" + amount + ", variety=" + variety + ", pesticides="
+				+ pesticides + ", fertilizers=" + fertilizers + ", createdDate=" + createdDate + "]";
+	}
+
+//////////////////////////HASHCODE EQUALS //////////////////////////////
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		GardenItem other = (GardenItem) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+	
+	
+
+	
+}
---
+++ JPACultivAid/src/test/java/com/skilldistillery/cultivaid/entities/GardenItemCommentTest.java
@@ -0,0 +1,89 @@
+package com.skilldistillery.cultivaid.entities;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class GardenItemCommentTest {
+
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private GardenItemComment gic; //Entity under test
+	
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("JPACultivAid"); // Before all tests - set up an EMF
+		
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		emf.close(); // After all tests, close the EMF
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		em = emf.createEntityManager(); // Before each test, get an EM
+		gic = em.find(GardenItemComment.class, 1); // Before each test get the User entity to test (test subject)
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		em.close(); // After each test close the EM
+		gic = null; // After each test set user back to null to prevent cross contamination between tests
+	}
+	
+	@Test
+	@DisplayName("TEST: GardenItemComment Basic Mappings")
+	void test1() {
+		assertNotNull(gic);
+		assertEquals("Wow these carrots look great.", gic.getContent());
+		assertEquals(LocalDateTime.of(2021, 8, 17, 0, 0, 0), gic.getCreatedDate());
+
+	}
+	
+	@Test
+	@DisplayName("TEST: Many GardenItemComment to One GardenItem Mapping")
+	void test2() {
+		assertNotNull(gic);
+		GardenItem gi = gic.getGardenItem();
+		assertNotNull(gi);
+		assertEquals(1, gi.getId());
+		assertEquals("Some delicious carrots.", gi.getDescription());
+	}
+	
+	
+	// Additional inserts added to DB for testing for TEST3 and TEST4
+	// Comment ID 1 is NOT in reply to any other comments
+	// Comment ID 2 is a reply to comment id 1
+	// Comment ID 3 is a reply to comment id 1 (a reply to comment 2 but the top level comment is #1)
+	@Test
+	@DisplayName("TEST: One GardenItemComment to Many GardenItemComments (replies)")
+	void test3() {
+		assertNotNull(gic.getReplies());
+		assertEquals(2, gic.getReplies().size());
+		assertEquals("a reply to the reply", gic.getReplies().get(1).getContent());
+	}
+	
+	@Test
+	@DisplayName("TEST: Many GardenItemComments to One GardenItemComment (the comment replied to)")
+	void test4() {
+		assertNull(gic.getInReplyToComment());
+		GardenItemComment gic2 = em.find(GardenItemComment.class, 2);
+		assertEquals(1, gic2.getInReplyToComment().getId());
+		assertEquals("Wow these carrots look great.", gic2.getInReplyToComment().getContent());
+	}
+}
