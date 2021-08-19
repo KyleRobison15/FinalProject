14652474bf530bc5d3507178417b7ba17c412b2d
Kyle Robison <kylerobison15@gmail.com>
2021-08-19 14:57:19 -0600

    Add update and delete user REST endpoints

--- CultivAid/src/main/java/com/skilldistillery/cultivaid/controllers/UserController.java
+++ CultivAid/src/main/java/com/skilldistillery/cultivaid/controllers/UserController.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -31,9 +32,44 @@ public class UserController {
 		return user;
 	}
 	
-	@PutMapping("users/password")
-	public User updateUserPassword(@RequestBody User user) {
-		return userSvc.updateUser(user);
+	@PutMapping("api/users/password")
+	public User resetPassword(@RequestBody User user, Principal principal) {
+		return userSvc.resetPassword(user);
+	}
+	
+	@PutMapping("api/users/{id}")
+	public User updateUserAccount(@RequestBody User user, @PathVariable("id") int userId, HttpServletResponse res, Principal principal) {
+		User userRequested = userSvc.findByUserId(userId);
+		
+		if (userRequested == null || !userRequested.getUsername().equals(principal.getName())) {
+			res.setStatus(404);
+			return null;
+		}
+		
+		User userUpdated = userSvc.updateUserAccount(user);
+		if (userUpdated == null) {
+			res.setStatus(400);
+		}
+		
+		return userUpdated;
+	}
+	
+	@DeleteMapping("api/users")
+	public boolean deleteUserAccount(@RequestBody User user, HttpServletResponse res, Principal principal) {
+		
+		boolean isDeleted = false;
+		
+		if (user.getUsername().equals(principal.getName())) {	
+			 isDeleted = userSvc.deleteUser(user);
+		}
+		
+		if (isDeleted) {
+			res.setStatus(204);
+		} else {
+			res.setStatus(404);
+		}
+		
+		return isDeleted;
 	}
 	
 }
--- CultivAid/src/main/java/com/skilldistillery/cultivaid/services/UserServiceImpl.java
+++ CultivAid/src/main/java/com/skilldistillery/cultivaid/services/UserServiceImpl.java
@@ -22,9 +22,19 @@ public class UserServiceImpl implements UserService {
 	public User findByUsername(String username) {
 		return userRepo.findByUsername(username);
 	}
+	
+	@Override
+	public User findByUserId(int uId) {
+		Optional<User> opt = userRepo.findById(uId);
+		
+		if(opt.isPresent()) {
+			return opt.get();
+		}
+		return null;
+	} 
 
 	@Override
-	public User updateUser(User user) {
+	public User resetPassword(User user) {
 		Optional<User> opt = userRepo.findById(user.getId());
 		User managed = null; 
 		if(opt.isPresent()) {
@@ -35,11 +45,48 @@ public class UserServiceImpl implements UserService {
 		
 		return userRepo.saveAndFlush(managed); 
 	}
+	
+	@Override
+	public User updateUserAccount(User user) {
+		Optional<User> opt = userRepo.findById(user.getId());
+		User managed = null; 
+		if(opt.isPresent()) {
+			managed = opt.get();
+			managed.setUsername(user.getUsername());
+			managed.setFirstName(user.getFirstName());
+			managed.setLastName(user.getLastName());
+			managed.setEmail(user.getEmail());
+			managed.setPhone(user.getPhone());
+			managed.setImageUrl(user.getImageUrl());
+			managed.setImageUrl(user.getImageUrl());
+			
+			managed.getAddress().setAddress(user.getAddress().getAddress());
+			managed.getAddress().setAddress2(user.getAddress().getAddress2());
+			managed.getAddress().setCity(user.getAddress().getCity());
+			managed.getAddress().setStateAbbr(user.getAddress().getStateAbbr());
+			managed.getAddress().setPostalCode(user.getAddress().getPostalCode());
+		
+		}
+		
+		try {
+			return userRepo.saveAndFlush(managed);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		} 
+	}
 
 	@Override
-	public boolean deleteUser(User user, int uId) {
-		// TODO Auto-generated method stub
+	public boolean deleteUser(User user) {
+		User userToDelete = userRepo.findByUsername(user.getUsername());
+		
+		if (userToDelete != null) {
+			userToDelete.setActive(false);
+			userRepo.saveAndFlush(userToDelete);
+			return true;
+		}
+		
 		return false;
-	} 
+	}
 
 }
