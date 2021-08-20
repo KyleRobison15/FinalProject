81a8bcd59ee4b157e9368c7e53f1c74d8232f0a4
Chris <cwriddle5@gmail.com>
2021-08-20 14:06:57 -0400

    public user profile/vendor page angular added

---
+++ ngCultivAid/src/app/models/exchange.ts
@@ -0,0 +1,38 @@
+import { User } from "./user";
+
+// export class Exchange {
+
+//   rating: number;
+// 	active: boolean;
+// 	buyerComment: string;
+// 	complete: boolean;
+// 	accepted: boolean;
+// 	exchangeDate: string;
+// 	createDate: string;
+// 	buyer: User;
+//   exchangeItems = [];
+//   exchangeImages = [];
+
+//   constructor(
+//     rating: number = 0,
+//     active: boolean = true,
+//     buyerComment: string = '',
+//     accepted: boolean = false,
+//     exchangeDate: string = '',
+//     createDate: string = '',
+//     buyer: User = new User(),
+//     exchangeItems: ExchangeItem[] = [],
+//     exchangeImages: ExchangeImages[] = []
+//   ){
+//     this.rating = rating;
+//     this.active = active;
+//     this.buyerComment = buyerComment;
+//     this.accepted = accepted;
+//     this.exchangeDate = exchangeDate;
+//     this.createDate = createDate;
+//     this.buyer = buyer;
+//     this.exchangeItems = exchangeItems;
+//     this.exchangeImages = exchangeImages;
+//   }
+
+// }
